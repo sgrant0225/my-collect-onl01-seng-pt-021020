a1bd57d079081e7ba31845486819dc326377c639
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-17 05:25:30 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,8 @@
 require 'pry'
 def my_collect(array)
-  new_array = array.upcase
  i = 0 
- while i < new_array.length do
-   yield(new_array[i])
+ while i < array.length do
+   yield(array[i])
    i += 1
  end
 end
