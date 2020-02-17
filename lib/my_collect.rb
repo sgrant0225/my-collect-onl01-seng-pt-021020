cc261195ef31cdc0f6cffde609e38a48c0d66e13
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-17 05:34:25 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+require 'pry'
+def my_collect(array)
+  i = 0 
+  new_collection = []
+ while i < array.length do
+   new_collection << yield(array[i])
+   i += 1
+ end
+ new_collection
+end
+  
